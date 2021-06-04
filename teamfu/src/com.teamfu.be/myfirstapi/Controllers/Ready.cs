e886bde54be4629f440924207bc85aee689629e9
MpumeleloPSN <17749402@vcconnect.co.za>
2021-06-04 20:35:01 +0200

    Up to task 2 of teamfu

---
+++ teamfu/src/com.teamfu.be/myfirstapi/Controllers/Ready.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace myfirstapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Ready : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+       
+    }
+}
+
